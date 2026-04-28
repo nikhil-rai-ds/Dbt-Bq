079bd85d29903ab9cddbdb847e93049b851b13b3
nikhil.dce.professional@gmail.com <nikhil.dce.professional@gmail.com>
2026-04-28 06:10:45 +0000

    adding dim_locaiton

--- models/marts/dim_location.sql
+++ models/marts/dim_location.sql
@@ -1,1 +1,6 @@
-select 1 
+select  cast(locationid as int) as location_id , 
+        borough as Borough, 
+        zone as Zone, 
+        service_zone as Service_Zone
+
+from {{ref('tax_zone_lookup')}}
