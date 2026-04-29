156fe69326f81531cf1745e12faa853139c47d68
nikhil.dce.professional@gmail.com <nikhil.dce.professional@gmail.com>
2026-04-29 07:19:09 +0000

    creating fct_trips with unique trip id and duplicate records are there because of different payment

--- analyses/analyse_int_union_trip_data.sql
+++ analyses/analyse_int_union_trip_data.sql
@@ -1,38 +1,17 @@
 with unique_trips as (
     select * from {{ref('int_union_tripdata')}} 
-     
+    limit 100
 )
 
-select 
- {{ dbt_utils.generate_surrogate_key([
-    'vendor_id',
-    'rate_code_id',
-    'pickup_location_id',
-    'dropoff_location_id',
-    'pickup_datetime',
-    'dropoff_datetime',
-    'passenger_count',
-    'trip_type',
-    'payment_type',
-    'taxi_type'
-]) }} AS trip_id, 
-vendor_id,rate_code_id,pickup_location_id,dropoff_location_id,
+select vendor_id,rate_code_id,pickup_location_id,dropoff_location_id,
          pickup_datetime,dropoff_datetime,passenger_count,trip_type,payment_type ,taxi_type 
          ,sum(fare_amount) as fare_amount, sum(extra) as extra, sum(mta_tax) mta_tax,sum(tip_amount) tip_amount
          ,sum(tolls_amount) tolls_amount,sum(improvement_surcharge) improvement_surcharge
          ,sum(total_amount)total_amount
-         ,sum(ehail_fee)ehail_fee
+         ,sum(congestion_surcharge)congestion_surcharge
 from  unique_trips 
 group by  vendor_id,rate_code_id,pickup_location_id,dropoff_location_id,pickup_datetime,dropoff_datetime ,passenger_count,
 trip_type,payment_type,taxi_type 
 
 
--- mount
--- extra
--- mta_tax
--- tip_amount
--- tolls_amount
--- ehail_fee
--- improvement_surcharge
--- total_amount
--- payment_type
+-- select * from  {{ref('int_union_tripdata')}}
--- models/intermediate/int_union_tripdata.sql
+++ models/intermediate/int_union_tripdata.sql
@@ -5,9 +5,9 @@ yellow as (
     select * from {{ref('stg_yellow_tripdata')}}
 )
 
-select *
+select * , "Green" as taxi_type
 from green 
 union  all
-select * 
+select * , "Yellow" as taxi_type
 from yellow
  
