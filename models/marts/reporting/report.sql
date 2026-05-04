abeb676e79ac4a1ca4e8fd0ad851e202e6a15c2a
Nikhil Rai <nikhil.dce.professional@gmail.com>
2026-05-04 07:24:09 +0000

    adding model contracts

--- models/marts/reporting/report.sql
+++ models/marts/reporting/report.sql
@@ -1,3 +1,9 @@
+{{ config(
+    materialized='incremental',
+    unique_key='trip_id',
+    contract={'enforced': true}
+) }}
+
 select fct.* , pay.payment_name
 from {{ref('fct_tripdata')}} fct 
 left join {{ref('dim_payment')}} pay on pay.payment_type = fct.payment_type
